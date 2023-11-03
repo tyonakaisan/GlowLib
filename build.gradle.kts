26f2bc6a7ea1d8f2ab61ba8d0ecd411db75a40c4
Tyonakaisan <mikanno82@gmail.com>
2023-11-03 17:18:05 +0900

    first

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("java")
+    id("maven-publish")
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://jitpack.io")
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    // Paper
+    compileOnly("io.papermc.paper", "paper-api", "1.20.2-R0.1-SNAPSHOT")
+}
+
+tasks {
+    compileJava {
+        this.options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group as String
+            artifactId = project.name
+            version = project.version as String
+
+            from(components["java"])
+        }
+    }
+}
---
+++ src/main/java/com/tyonakaisan/glowlib/GlowLib.java
@@ -0,0 +1,12 @@
+package com.tyonakaisan.glowlib;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.framework.qual.DefaultQualifier;
+
+@DefaultQualifier(NonNull.class)
+public final class GlowLib {
+
+    public GlowLib() {
+
+    }
+}
