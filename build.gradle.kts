9759a38724173fc82c922f4566808d7fae8a28ec
Tyonakaisan <mikanno82@gmail.com>
2023-11-03 17:54:33 +0900

    first

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 
 dependencies {
     // Paper
-    compileOnly("io.papermc.paper", "paper-api", "1.20.2-R0.1-SNAPSHOT")
+    //compileOnly("io.papermc.paper", "paper-api", "1.20.2-R0.1-SNAPSHOT")
 }
 
 tasks {
